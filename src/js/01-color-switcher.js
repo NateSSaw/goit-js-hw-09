06bba3d09e09cb406d8d240ab216dc05b79441f4
Nataliia Semenova <natasemenova1806@gmail.com>
2023-01-21 00:52:55 +0200

    first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const bodyEl = document.querySelector('body');
 
+stopBtn.disabled = true;
+let timerChange = null;
+startBtn.addEventListener('click', () => {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+
+  timerChange = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerChange);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+});
